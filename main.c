d536f6f8e12a4243ddf64b497b9fe28bfd791f42
Valentin Nussbaumer <valentin.nussbaumer@noseryoung.com>
2022-10-05 16:03:23 +0200

    Updated

--- main.c
+++ main.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
-#include "calculate.h"
-
-#define RESULT;
-int main() {
-int c;
-c = 6;
-printf("Welcome to the best programm ever. \nEnter your date of birth to proceed. I thnk thath you are 6. Years old.");
-//printf("%d", c)
-if(c == 6){ printf("I knew it");}
-else{ printf("Sorry"); }
-for(int i = 0; i < 6: i--){ printf("You were %d Year old", i);}
-
-
-printf("Thanks for using the best Programm in the world");
-return 0;
-}
+#include <stdio.h>#include "calculate.h"#define RESULT;int main() {int c;c = 6;printf("Welcome to the best programm ever. \nEnter your date of birth to proceed. I thnk thath you are 6. Years old.");//printf("%d", c)if(c == 6){ printf("I knew it");}else{ printf("Sorry"); }for(int i = 0; i < 6: i--){ printf("You were %d Year old", i);}printf("Thanks for using the best Programm in the world");return 0;}
